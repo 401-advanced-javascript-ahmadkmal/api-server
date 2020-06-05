7983523135836e5a6c8f62194556fb17c7d30034
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-06-05 21:00:03 +0300

    add test for all routes

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -5,17 +5,20 @@ const supergoose = require('@code-fellows/supergoose');
 const mockRequest = supergoose(server);
 
 describe('categories API', () => {
+  var id ;
   
   it('can post()', () => {
-    const obj = { name: 'orange', description: 'color' };
+    const obj = { name: 'hakona', description: 'batata' };
     return mockRequest
       .post('/api/v1/categories')
       .send(obj)
       .then((data) => {
         const record = data.body; // _id
+        console.log('this recorde to get id',record._id);
         Object.keys(obj).forEach((key) => {
           expect(record[key]).toEqual(obj[key]);
         });
+        id = data.body._id;
       });
   });
 
@@ -27,15 +30,66 @@ describe('categories API', () => {
       .then((data) => {
         return mockRequest.get('/api/v1/categories').then((result) => {
           Object.keys(obj).forEach((key) => {
-            expect(result.body.results[0][key]).toEqual(obj[key]);
+            expect(result.body.results[1][key]).toEqual(obj[key]);
           });
         });
       });
   });
+  it('can get() by id', () => {
+    const obj = { name: 'hakona', description: 'batata' };
+    return mockRequest
+      .get(`/api/v1/categories/${id}`)
+      .then((data) => {
+        console.log('can get() by id result',data.body);
+        Object.keys(obj).forEach((key) => {
+          expect(data.body[0][key]).toEqual(obj[key]);
+        });
+      });
+  });
+
+  it('can put() by id', () => {
+    const obj = { name: 'noName' };
+    const obj2 = { name: 'noName', description: 'batata' };
+    return mockRequest
+      .put(`/api/v1/categories/${id}`)
+      .send(obj)
+      .then((data) => {
+        console.log('can put() by id result',data.body);
+        return mockRequest.get(`/api/v1/categories/${id}`).then((result) => {
+          Object.keys(obj2).forEach((key) => {
+            expect(result.body[0][key]).toEqual(obj2[key]);
+          });
+          
+        });
+        
+      });
+      
+  });
+  it('can delete() by id', () => {
+    const obj2 = { name: 'noName', description: 'batata' };
+    return mockRequest
+      .delete(`/api/v1/categories/${id}`)
+      .then((data) => {
+
+       
+        Object.keys(obj2).forEach((key) => {
+          expect(data.body[key]).toEqual(obj2[key]);
+        });
+
+        return mockRequest.get('/api/v1/categories').then((result) => {
+          console.log('can delete() by id result',result.body);
+          result.body.results.forEach((element) => {
+            expect(element._id).not.toEqual(id);
+          });
+        });
+      });
+      
+  });
 });
 
+
 describe('products API', () => {
-  
+  var id ;
   it('can post()', () => {
     const obj = {  name: 'hakona', category: 'batata',description: 'temon and pomba',price: '2$' };
     return mockRequest
@@ -43,7 +97,7 @@ describe('products API', () => {
       .send(obj)
       .then((data) => {
         const record = data.body; // _id
-        
+        id = data.body._id;
         Object.keys(obj).forEach((key) => {
           expect(record[key]).toEqual(obj[key]);
         });
@@ -51,7 +105,7 @@ describe('products API', () => {
   });
   
   it('can get()', () => {
-    const obj = { name: 'hakona', category: 'batata',description: 'temon and pomba',price: '2$' };
+    const obj = { name: 'batata', category: 'hakona',description: 'temon and pomba',price: '2$' };
     return mockRequest
       .post('/api/v1/products')
       .send(obj)
@@ -59,9 +113,59 @@ describe('products API', () => {
         return mockRequest.get('/api/v1/products').then((result) => {
           console.log('data',result.body.results[0]);
           Object.keys(obj).forEach((key) => {
-            expect(result.body.results[0][key]).toEqual(obj[key]);
+            expect(result.body.results[1][key]).toEqual(obj[key]);
           });
         });
       });
+  });
+  it('can get() by id', () => {
+    const obj = {  name: 'hakona', category: 'batata',description: 'temon and pomba',price: '2$' };
+    return mockRequest
+      .get(`/api/v1/products/${id}`)
+      .then((data) => {
+        console.log('can get() by id result',data.body);
+        Object.keys(obj).forEach((key) => {
+          expect(data.body[0][key]).toEqual(obj[key]);
+        });
+      });
+  });
+
+  it('can put() by id', () => {
+    const obj = { name: 'noName' };
+    const obj2 = {  name: 'noName', category: 'batata',description: 'temon and pomba',price: '2$' };
+    return mockRequest
+      .put(`/api/v1/products/${id}`)
+      .send(obj)
+      .then((data) => {
+        console.log('can put() by id result',data.body);
+        return mockRequest.get(`/api/v1/products/${id}`).then((result) => {
+          Object.keys(obj2).forEach((key) => {
+            expect(result.body[0][key]).toEqual(obj2[key]);
+          });
+          
+        });
+        
+      });
+      
+  });
+  it('can delete() by id', () => {
+    const obj2 = {  name: 'noName', category: 'batata',description: 'temon and pomba',price: '2$' };
+    return mockRequest
+      .delete(`/api/v1/products/${id}`)
+      .then((data) => {
+
+       
+        Object.keys(obj2).forEach((key) => {
+          expect(data.body[key]).toEqual(obj2[key]);
+        });
+
+        return mockRequest.get('/api/v1/products').then((result) => {
+          console.log('can delete() by id result',result.body);
+          result.body.results.forEach((element) => {
+            expect(element._id).not.toEqual(id);
+          });
+        });
+      });
+      
   });
 });
