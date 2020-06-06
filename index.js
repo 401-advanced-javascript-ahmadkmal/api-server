648be779816e241e5edfc9aaf5bba2b02a410e5e
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-06-06 16:38:32 +0300

    add jsdoc

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const server = require('./lib/server.js');
-
+/**
+ * Simple configration 
+ * connect to mongodb
+ * @module index
+ */
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
